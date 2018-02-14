74574dc572748180ebebf9fffcd3d51dd4aff577
D062429 <vincent.manz@sap.com>
2018-02-14 09:21:29 +0000

    java coding

--- ToDo/src/main/java/com/company/todo/ToDoService/ToDoService.java
+++ ToDo/src/main/java/com/company/todo/ToDoService/ToDoService.java
@@ -1,9 +1,26 @@
 package com.company.todo.ToDoService;
 
-/**
- *
- * @author D062429
- */
-public class ToDoService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class ToDoService extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(ToDoService.class);
+
+    @Override
+    protected void doGet( final HttpServletRequest request, final HttpServletResponse response )
+        throws ServletException, IOException
+    {
+        logger.info("I am running!");
+        response.getWriter().write("Hello World!");
+    }
 }
